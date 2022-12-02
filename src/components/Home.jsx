4b84b07121e239468e0182aafbbba87fdd34e177
tilahun-tadesse <119629930+tilahun-tadesse@users.noreply.github.com>
2022-12-02 10:28:19 +0300

    update home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Programing student
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
              I learn 2 years of  building and desgining software.
